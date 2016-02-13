a5bb63e04f3e04b238d8c6e5ce932d5b97d07252
19thyneb <thyne.boonmark@gmail.com>
2016-02-12 19:08:13 -0500

    Circuit Game Prototype
    Click on tiles to rotate them. Goal is to connect the green box to all
    of the red boxes.

---
+++ The Experiment/Assets/CircuitGame/Scripts/LineDetector.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineDetector : MonoBehaviour 
+{
+	public bool isConnected = false;
+	public bool isBox = false;
+	public bool isCross = false;
+
+	public GameObject connectedTo;
+	private SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () 
+	{
+		sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (connectedTo != null) {
+			if (connectedTo.GetComponent<LineDetector> ().isConnected == false) {
+				isConnected = false;
+				connectedTo = null;
+			}
+		}
+		if (isCross) {
+			if (isConnected) {
+				Transform[] children = new Transform[transform.childCount];
+				for (int i = 0; i < transform.childCount; i++) {
+					children [i] = transform.GetChild (i);
+				}
+				foreach (Transform child in children){
+					SpriteRenderer sprite = child.GetComponent<SpriteRenderer>();
+					sprite.color = new Color (255, 255, 255, 1);
+				}
+			}else{
+				Transform[] children = new Transform[transform.childCount];
+				for (int i = 0; i < transform.childCount; i++) {
+					children [i] = transform.GetChild (i);
+				}
+				foreach (Transform child in children){
+					SpriteRenderer sprite = child.GetComponent<SpriteRenderer>();
+					sprite.color = new Color (255, 255, 255, .3f);
+				}
+			}
+		}
+		else {
+			if (isConnected) {
+				sprite.color = new Color (255, 255, 255, 1);
+			} else {
+				sprite.color = new Color (255, 255, 255, .3f);
+			}
+		}
+	}
+
+	void OnTriggerStay2D (Collider2D other)
+	{
+		if (connectedTo == null) {
+			if (other.gameObject.GetComponent<LineDetector> () != null) {
+				if (other.gameObject.GetComponent<LineDetector> ().isConnected == true) {
+					if (other.gameObject.GetComponent<LineDetector> ().connectedTo != this.gameObject) {
+						isConnected = true;
+						connectedTo = other.gameObject;
+					}
+				}
+			}
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other)
+	{
+		if (!isBox) {
+			if (isConnected) {
+				connectedTo = null;
+				isConnected = false;
+			}
+		}
+	}
+}
