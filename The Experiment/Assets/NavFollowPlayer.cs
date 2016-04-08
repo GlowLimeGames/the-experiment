6036b2f2e2f37eff99ff2e968f35bef5a4ee2252
Jowell-E <jjoelesquilin@gmail.com>
2016-04-08 18:17:10 -0400

    Tolstoy Follow Player Nav First Room
    Very simple follow bool on Tolstoy set to true to start (or when player
    leaves his radius)

---
+++ The Experiment/Assets/NavFollowPlayer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavFollowPlayer : MonoBehaviour {
+
+	public GameObject target;
+	public bool follow = false;
+
+	private Animator anim;
+	private NavMeshAgent nav;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		nav = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (follow) {
+			Vector3 targetDir = target.transform.position - transform.position;
+			Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, 1.5f * Time.deltaTime, 0.0F);
+			transform.rotation = Quaternion.LookRotation(newDir);
+
+			nav.SetDestination (target.transform.position);
+			anim.SetBool ("Walking", true);
+		} else {
+			anim.SetBool ("Walking", false);
+		}
+
+		if (follow) {
+			nav.Resume ();
+		} else {
+			nav.Stop ();
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Player")) {
+			follow = false;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.gameObject.CompareTag ("Player")) {
+			follow = true;
+		}
+	}
+}
